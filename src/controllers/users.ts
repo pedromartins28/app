5d276d11dde591abaeee55ebf8a4256943bdb9b6
pedromartins28 <pedromartins28023@gmail.com>
2023-12-05 22:07:39 -0300

    create app_useerr

---
+++ src/controllers/users.ts
@@ -0,0 +1,20 @@
+import {Elysia} from "elysia";
+import authenticate from "../middlewares/authenticate";
+import { getUser, createUser, updateUser, delUser } from "../services/users";
+import { createUserSchema, delUserSchema, updateUserSchema } from "../dtos/users";
+
+const users = new Elysia({prefix: "/users"}).get("/", function(context){
+    const user = authenticate(context)
+    return getUser({id: user.id})
+}).post("/", function(context){
+    const user = authenticate(context)
+    return createUser(context.body)
+}, {body: createUserSchema}).put("/", function(context){
+    const user = authenticate(context)
+    return updateUser(context.body)
+}, {body: updateUserSchema}).delete("/:id", function(context){
+    const user = authenticate(context)
+    return delUser(context.params)
+}, {params: delUserSchema})
+
+export default users
